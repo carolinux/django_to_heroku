abb1174756a186c8b59938b5c3b33dd48ed1943f
carolinux <carolinegr@gmail.com>
2020-01-27 16:11:10 +0100

    Add static image

--- news_ticker/news_ticker/settings.py
+++ news_ticker/news_ticker/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# location(s) of static files locally
+STATICFILES_DIRS = [os.path.join('news_ticker', 'static')]
--- news_ticker/news_ticker/views.py
+++ news_ticker/news_ticker/views.py
@@ -8,15 +8,14 @@ from news_ticker import settings
 
 class News(View):
     """
-    Return top N news in json format
+    Return top N news titles in json format
     """
     def get(self, request):
         n = request.GET.get('n', 5)
         guardian_url = "https://content.guardianapis.com/search?api-key={}&page-size={}".format(settings.GUARDIAN_API_KEY, n)
         resp = requests.get(guardian_url)
         resp_json = resp.json()
-        print(resp_json)
-        return JsonResponse({'titles': [article['webTitle'] for article in resp_json['response']['results']]})
+        return JsonResponse({'titles': [{"title":article['webTitle'], "url":article['webUrl']} for article in resp_json['response']['results']]})
 
 
 class Home(TemplateView):
